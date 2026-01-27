53bb0073e36362f374aadf447478189496ab3b6d
Samkeed <nwekechidimma88@gmail.com>
2026-01-28 00:13:07 +0100

    created the folders for the auth and user

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { datasourceOptions } from './database/data-source';
+import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
 @Module({
   imports: [    
-    TypeOrmModule.forRoot(datasourceOptions),
+    TypeOrmModule.forRoot(datasourceOptions), UserModule, AuthModule,
   
   ],
   controllers: [AppController],
